4f35f507726baf390e4f20b3f966b4fd007335d6
MrCrayfish <mrcrayfish@hotmail.com>
2021-05-26 19:23:08 +0930

    Basic mod setup 🔨

---
+++ src/main/java/com/mrcrayfish/morefurniture/MoreFurnitureMod.java
@@ -0,0 +1,17 @@
+package com.mrcrayfish.morefurniture;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+/**
+ * Author: MrCrayfish
+ */
+@Mod(Reference.MOD_ID)
+public class MoreFurnitureMod
+{
+    public MoreFurnitureMod()
+    {
+
+    }
+}
---
+++ src/main/java/com/mrcrayfish/morefurniture/Reference.java
@@ -0,0 +1,9 @@
+package com.mrcrayfish.morefurniture;
+
+/**
+ * Author: MrCrayfish
+ */
+public class Reference
+{
+    public static final String MOD_ID = "morecfm";
+}
